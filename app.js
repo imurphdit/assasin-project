84140caa99be241e4f4a2dd1c1291c7d9e0da742
imurphd.it <masinimurphy@icloud.com>
2025-06-14 16:26:31 -0400

    added error handling

--- app.js
+++ app.js
@@ -43,87 +43,163 @@ app.get('/login', (req, res) => {
 
 // REDIRECT TO AGENT PAGE VIA PIN
 app.post('/login', async (req, res) => {
-  const pin = req.body;
-  const agent = await Agent.findOne({ where: pin});
-  
-  if (agent) {
-    req.session.agent = agent.name
-    req.session.agentID = agent.id
-    res.redirect('/agent/' + agent.id.toString());
-  } else {
-    res.sendStatus(404);
+  try{
+    const pin = req.body;
+    const agent = await Agent.findOne({ where: pin});
+    
+    if(!agent){
+      return res.status(404).json({
+        error: 'User not found',
+        message: 'No agent found with matching pin.'
+      })
+    } else {
+        req.session.agent = agent.name
+        req.session.agentID = agent.id
+        res.redirect('/agent/' + agent.id.toString());
+    }
+  } catch (err) {
+    console.error('Error loggin in: ', err)
+    res.status(500).json({
+      error: 'Internal server error',
+      message: 'Failed to redirect to login'
+    })
   }
 });
 
 
 //LOAD AGENT PAGE VIA UUID
 app.get('/agent/:id', auth, async (req, res) => {
-  const agent = await Agent.findByPk(req.params.id)
-  
-  //IF AGENT ALIVE, LOAD
-  if (agent && agent.isdead === false){
-    if (agent.won === true){
-      res.render('win')
-    } else {
-      res.render('agentPage', { img: agent.img, target: agent.target, pin: agent.agentpin, name: agent.name });
+  try{
+    const agent = await Agent.findByPk(req.params.id)
+
+    if(!agent){
+      return res.status(404).json({
+        error: 'Agent not found',
+        message: 'No agent found with this UUID'
+      })
     }
-  } else {
-    res.sendStatus(404);
+
+
+    if(agent.isdead) {
+      return res.status(404).json({
+        error: 'Failed to log in',
+        message: 'This agent has been eliminated'
+      })
+    }
+
+    if(agent.won){
+        res.render('win')
+    } else {
+        res.render('agentPage', {img: agent.img, target: agent.target, pin: agent.agentpin, name: agent.name })
+    }
+
+  } catch (err){
+    console.error('', err)
+    res.status(500).json({
+      error: 'Internal server error',
+      message: 'Failed to find agent via UUID'
+    })
   }
 });
 
 
 //GET AGENTS LIST
 app.get('/api/agent/', auth, async (req, res) => {
-  const agents = await Agent.findAll();
-  res.json(agents); 
+  try {
+    const agents = await Agent.findAll();
+    res.json(agents)
+
+  } catch (err){
+    console.error('', err)
+    res.status(500).json({
+      error: 'Internal server error',
+      message: 'Failed to list all Agents'
+    })
+  }
 });
 
 
 //CREATE AGENT 
 app.post('/api/agent/', async (req, res) => {
-  const agent = await Agent.create(req.body);
-  res.json(agent);
+  try{
+    const agent = await Agent.create(req.body);
+    res.json(agent);
+
+  } catch (err){
+    console.error('', err)
+    res.status(500).json({
+      error: 'Internal server error',
+      message: 'Failed to create new agent'
+    })
+  }
 });
 
 
 //KILL AGENT
 app.post('/api/kill/:target', auth, async (req, res) => {
-  const agent = await Agent.findByPk(req.session.agentID)
-  const agentKilled = await Agent.findOne( { where: { name: req.params.target }})
-  const newTarget = await Agent.findAll({
-    where: {
-      isdead: false,
-      [Op.not]: [
-        { name: [req.session.agent, req.params.target]}
-      ]
-    },
-    order: sequelize.random(),
-    limit: 1
-  })
+  try{
+    const agent = await Agent.findByPk(req.session.agentID)
+    const agentKilled = await Agent.findOne( { where: { name: req.params.target }})
+    const newTarget = await Agent.findAll({
+      where: {
+        isdead: false,
+        [Op.not]: [
+          { name: [req.session.agent, req.params.target]}
+        ]
+      },
+      order: sequelize.random(),
+      limit: 1
+    })
 
-  //CHECK IF THIS IS THEIR TARGET
-  if(agent.target === agentKilled.name){
-    agentKilled.isdead = true;
-    agentKilled.killedby = agent.name;
-    await agentKilled.save();
-    console.log(req.session.agent + ' has eliminated ' + agentKilled.name)
-  } else {
-    console.log(req.session.agent + ' attempted an unauthorized kill...')
-    return res.send('This isnt your target...')
-  }
+    if(!agent){
+      return res.status(404).json({
+        error: 'Unable to find Agent',
+        message: 'Does the agentID exist?'
+      })
+    }
 
-  //CHECK IF THERE ARE ANY PLAYERS LEFT
-  if(newTarget.length === 0){
-    agent.won = true;
-    await agent.save();
-    console.log(req.session.agent + ' has won the game. Now let us see if they are worthy..')
-    res.render('win');
-  } else{
-    console.log(req.session.agent + ' is given a new target.')
-    agent.target = newTarget[0].name;
-    await agent.save();
-    res.redirect(`/agent/${agent.id.toString()}`)
+    if(!agentKilled){
+      return res.status(404).json({
+        error: 'Unable to find target',
+        message: 'Does the target exist?'
+      })
+    }
+
+    //CHECK IF THIS IS THEIR TARGET
+    if(agent.target === agentKilled.name){
+      
+      //KILL THEM
+      agentKilled.isdead = true;
+      agentKilled.killedby = agent.name;
+      await agentKilled.save();
+
+      console.log(req.session.agent + ' has eliminated ' + agentKilled.name)
+
+    } else {
+      return res.status(401).json({
+        error: 'Unauthorized',
+        message: 'You are not authorized to kill this Agent'
+      })
+    }
+
+    //CHECK IF THERE ARE ANY PLAYERS LEFT
+    if(newTarget.length === 0){
+      agent.won = true;
+      await agent.save();
+      console.log(req.session.agent + ' has won the game. Now let us see if they are worthy..')
+      res.render('win');
+    } else{
+      console.log(req.session.agent + ' is given a new target.')
+      agent.target = newTarget[0].name;
+      await agent.save();
+      res.redirect(`/agent/${agent.id.toString()}`)
+    }
+  } catch (err) {
+    console.error('', err)
+    res.status(500).json({
+      error: 'Internal service error',
+      message: 'Failed to kill agent'
+    })
   }
 });
 
